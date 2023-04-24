9f7983d59973b06ced4dd02b58eba88da999d5c4
Bhawani Singh <bhawani.singh@abjima.com>
2023-04-24 10:37:23 +0530

    New Commit

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -25,19 +25,24 @@ namespace CityInfo.API.Controllers
             var cityEntities = await _cityInfoRepository.GetCitiesAsync();
 
             return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
-            //return Ok(_citiesDataStore.Cities);
+            
         }
         [HttpGet("id")]
-        public ActionResult <CityDto> GetCity(int id) 
-        {   //find city
-            //var cityToReturn = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
+        public async Task<IActionResult> GetCity(
+            int id, bool includePointsOfInterest = false) 
+        {
+            var city = await _cityInfoRepository.GetCityAsync(id, includePointsOfInterest);
+            if (city == null)
+            {
+                return NotFound();
+            }
 
-            //if (cityToReturn == null) 
-            //{
-            //     return NotFound();
-            //}
-            // return Ok(cityToReturn); 
-            return Ok();
+            if (includePointsOfInterest)
+            {
+                return Ok(_mapper.Map<CityDto>(city));
+            }
+
+            return Ok(_mapper.Map<CityWithoutPointsOfInterestDto>(city));
         }
     }
 }
